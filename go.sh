121a1b676741cbbea38f58ac978ea953f916f4bc
Dan Siwiec <daniel.siwiec@gmail.com>
2019-03-18 22:15:53 -0700

    Add mongo service definition

--- go.sh
+++ go.sh
@@ -2,4 +2,4 @@ git clone https://github.com/todo-deploy/todo-app.git
 mv todo-app/* .
 rm -rf todo-app/
 docker build . -t todo-app
-kubectl apply -f deployment-app.yaml
+kubectl apply -f deployment-db.yaml,deployment-app.yaml
